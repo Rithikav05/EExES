e630942f58af495b38d214897412c594c1b1fd60
Rithikav05 <145677036+Rithikav05@users.noreply.github.com>
2024-05-14 09:59:49 +0530

    Flex12

--- ldrandflex.ino
+++ ldrandflex.ino
@@ -1,14 +1,9 @@
 #include<Servo.h>
 #include<stdlib.h>
-#define M0 0
-#define M1 1
-#define M2 2
-#define M3 3
+
 #define mean 90
-#define ldr A0
 #define TRUE 1
 #define FALSE 0
-
 
 int exct;
 int pos=0;
@@ -21,11 +16,11 @@ Servo myservo3;
 void setup()
 {  
   Serial.begin(9600);
-  pinMode(ldr,INPUT);
-  myservo0.attach(M0);
-  myservo1.attach(M1);
-  myservo2.attach(M2);
-  myservo3.attach(M3);
+  pinMode(A0,INPUT);
+  myservo0.attach(4);
+  myservo1.attach(5);
+  myservo2.attach(6);
+  myservo3.attach(7);
 
   pos=0;
 }
@@ -33,23 +28,32 @@ void setup()
 void loop()
 {
 int ldrval=analogRead(ldr); 
-if(ldrval<=30)
+if(ldrval<=25)
   {
     exct=TRUE;
     Serial.print("Pick-up. ");
     Serial.println(ldrval);
-    Serial.println(exct);
+    //Serial.println(exct);
 
   topickup();
   delay(1000);
-  flex();
+    
+  flex1();
+  pickGrip();
+  flex2();
   delay(1000);
+  
   torest(180);
   delay(1000);
+  
   todrop();
   delay(1000);
-  flex();
+  
+  flex1();
+  dropGrip();
+  flex2();
   delay(1000);
+  
   torest(0);
   delay(1000);
 }
@@ -58,7 +62,7 @@ else
     exct=FALSE;
     Serial.print("Rest. ");
     Serial.println(ldrval);
-    Serial.println(exct);
+    //Serial.println(exct);
     
   }
   delay(1000);
@@ -102,33 +106,57 @@ void todrop()
   }
 }
 
-void flex()
+void flex1()
 {
-  for(int i=90,j=90;i<=130;i++,j--)
+  for(int j=90;j>=60;j--)
   {
     myservo1.write(j);
     myservo2.write(j);
     delay(30);
   }
   delay(1000);
-  for(int k=160;k>=30;k--)
+  for(int j=60;j>=50;j--)
+  {
+    myservo2.write(j);
+    delay(30);
+  }
+  delay(500);
+}
+
+void flex2()
+{
+  for(int j=50;j<=60;j++)
+  {
+    myservo2.write(j);
+    delay(30);
+  }
+  delay(600);
+  for(int j=60;j<=90;j++)
+  {
+    myservo1.write(j);
+    myservo2.write(j);
+    delay(30);
+  }
+  delay(1000);
+}
+
+
+void pickGrip()
+{
+  for(int k=30;k<=165;k++)
+  {
+    myservo3.write(k);
+    delay(30);
+  }
+  delay(1000);
+}
+
+void dropGrip()
+{
+  for(int k=165;k>=30;k--)
   {
     myservo3.write(k);
     delay(30);
   }
   delay(900);
-  for(int k=30;k<=160;k++)
-  {
-    myservo3.write(k);
-    delay(30);
-  }
-  delay(1000);
-  for(int i=130,j=50;i>=90;i--,j++)
-  {
-    myservo1.write(j);
-    myservo2.write(j);
-    delay(30);
-  }
-  delay(1000);
-
 }
